d0b8fa9d9bc9e6be4c51b3089ea0962ef8a0b93a
brylimz <superuser@brylimz.com>
2021-12-20 16:07:15 +0700

    lanjut rumah

--- main.py
+++ main.py
@@ -7,14 +7,23 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-
+window.config(padx=20, pady=20, bg="white", highlightthickness=0)
 canvas = Canvas(width=200, height=200, bg="white")
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.create_image(100, 100, image=logo_img,  )
+canvas.grid(column=2, row=1)
+
+website_label = Label(text="Website", fg="black")
+website_label.grid(column=0, row=2)
+website_text = Text(height=1, width=35)
+website_text.grid(column=2, row=2)
 
 
+email_username = Label(text="Email/Username", fg="black")
+email_username.grid(column=0, row=3)
 
+password = Label(text="Password", fg="black")
+password.grid(column=0, row=4)
 
 
 window.mainloop()
