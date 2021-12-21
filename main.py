2bf7984213475cf9d2936f85e646c02ae7fa4135
brylimz <superuser@brylimz.com>
2021-12-21 09:03:32 +0700

    in the mourning

--- main.py
+++ main.py
@@ -14,17 +14,36 @@ canvas.create_image(100, 100, image=logo_img,)
 canvas.grid(column=1, row=0)
 
 #labels
-website_label = Label(text="Website")
+website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 
-email_label = Label(text="Email/Username")
+email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
 
-password_label = Label(text="Password")
+password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #entries
 
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1)
+website_entry.focus()
+
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1)
+email_entry.insert(0, "superuser@brylimz.com")
+
+
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
+
+
+#buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=30)
+add_button.grid(row=4, column=1)
 
 
 window.mainloop()
