985759f349ce802e115914156eaba474e60027e3
brylimz <superuser@brylimz.com>
2021-12-21 09:24:37 +0700

    resign

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-
     with open("data.txt", mode="a") as data_file:
-        data_file.write(f"{website} | {email} | {password}")
+        data_file.write(f"{website} | {email} | {password}\n")
         website_entry.delete(0, END)
         password_entry.delete(0, END)
 
@@ -45,13 +44,15 @@ email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1)
 email_entry.insert(0, "superuser@brylimz.com")
 
-password_entry = Entry(width=21)
+password_entry = Entry(width=36)
 password_entry.grid(column=1, row=3)
 
 
 #buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(column=2, row=3)
+
+
 
 add_button = Button(text="Add", width=30)
 add_button.grid(row=4, column=1)
