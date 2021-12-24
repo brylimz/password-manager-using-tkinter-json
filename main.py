7dcc9fba0a7a8c159799dd3130d2b81e428a6243
brylimz <superuser@brylimz.com>
2021-12-24 15:37:20 +0700

    Handling Exceptions

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-#Password Generator Project
+# Password Generator Project
 
 
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
     password_letters = [choice(letters) for _ in range(randint(8, 10))]
@@ -24,6 +29,8 @@ def generate_password():
     password = "".join(password_list)
     password_entry.insert(0, password)
     pyperclip.copy(password)
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
@@ -32,15 +39,33 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+
+        }}
+
     if (len(website) and len(email) and len(password)) == 0:
         messagebox.showwarning(title="Oops", message="Please dont leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \n Email:{email} \n Password:{password}\n is it ok to save?")
-        if is_ok:
-            with open("data.txt", mode="a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open("data.json", mode="r") as data_file:
+                # reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # updating old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # saving the updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -49,10 +74,10 @@ window.title("Password Manager")
 window.config(padx=20, pady=20, bg="white", highlightthickness=0)
 canvas = Canvas(width=200, height=200, bg="white")
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo_img,)
+canvas.create_image(100, 100, image=logo_img, )
 canvas.grid(column=1, row=0)
 
-#labels
+# labels
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 
@@ -62,7 +87,7 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-#entries
+# entries
 
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1)
@@ -75,14 +100,12 @@ email_entry.insert(0, "superuser@brylimz.com")
 password_entry = Entry(width=36)
 password_entry.grid(column=1, row=3)
 
-#buttons
+# buttons
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(column=2, row=3)
-
 
 add_button = Button(text="Add", width=30)
 add_button.grid(row=4, column=1)
 add_button.config(command=save)
 
 window.mainloop()
-
