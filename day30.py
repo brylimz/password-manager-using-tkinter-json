286d5db8f8ffbf0121448e0f527c8d9fee2afba7
brylimz <superuser@brylimz.com>
2021-12-24 13:42:15 +0700

    started course

---
+++ day30.py
@@ -0,0 +1,15 @@
+# catch exception
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    raise TypeError("This is an error that i made up")
