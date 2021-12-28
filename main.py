4b2dfa2612dfcc34125ee8adaaf3dca01fa38af0
brylimz <superuser@brylimz.com>
2021-12-28 14:00:52 +0700

    completed code

--- main.py
+++ main.py
@@ -73,11 +73,18 @@ def save():
 
 def find_password():
     website = website_entry.get()
-    with open("data.json") as data_file:
-        data = json.load(data_file)
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data File Found")
+    else:
         if website in data:
             email = data[website]["email"]
             password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for{website} exists.")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
